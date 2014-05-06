313f876a6d988c0683b672c0dfe9eaa5bd951ae7
Mahmoud Mabrouk <mahmoud.mabrouk@tu-berlin.de>
2014-05-06 17:09:41 +0200

    Post

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-Configuration file: /home/mmabrouk/Documents/mmabrouk.github.io/_config.yml
-            Source: /home/mmabrouk/Documents/mmabrouk.github.io
-       Destination: /home/mmabrouk/Documents/mmabrouk.github.io/_site
-      Generating... done.
+jekyll build
+git add --all
+git commit -m "Post"
+git push origin master
